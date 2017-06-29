a3cc364f494246b0c77f8ce9841e62dc31d300c0
Henry Harrington <henry.harrington@gmail.com>
2017-06-29 22:39:13 +0100

    Improve collision response

--- Assets/Scripts/Game/Components/Collider.cs
+++ Assets/Scripts/Game/Components/Collider.cs
@@ -8,11 +8,13 @@ public class Collider: Component {
     public DReal height;
     public DReal radius;
     public bool fixedPosition;
+    public bool pushy;
 
     public Collider(Entity entity, ComponentPrototype proto): base(entity) {
         height = DReal.Parse(proto.data["height"]);
         radius = DReal.Parse(proto.data["radius"]);
         fixedPosition = proto.data["fixedPosition"] == "true";
+        pushy = proto.data["pushy"] == "true";
     }
 
     public override void OnCreate() {
@@ -24,14 +26,15 @@ public class Collider: Component {
             return;
         }
         var other_collider = other.GetComponent<Collider>();
+        if(!other_collider.pushy) {
+            return;
+        }
         var max_dist = radius + other_collider.radius;
-        var max_dist_sqr = max_dist * max_dist;
-        var sqr_dist = World.current.map.DistanceSqr(new DVector3(other.position.x, 0, other.position.z),
-                                                     new DVector3(entity.position.x, 0, entity.position.z));
-        var punt_power = max_dist_sqr / (max_dist_sqr - sqr_dist) / 2;
+        var dist = World.current.map.Distance(new DVector3(other.position.x, 0, other.position.z),
+                                              new DVector3(entity.position.x, 0, entity.position.z));
         var dir = World.current.map.Direction(new DVector3(other.position.x, 0, other.position.z),
                                               new DVector3(entity.position.x, 0, entity.position.z));
-        entity.position += dir * punt_power;
+        entity.position += dir * (max_dist - dist) / 2;
     }
 
     public override void OnDestroy() {
