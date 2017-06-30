de520d11a28b8aff65457e0ae3f5fe49d6ce41f1
Henry Harrington <henry.harrington@gmail.com>
2017-06-30 18:49:30 +0100

    Some kind of placeable shield thing?

--- Assets/Bob.cs
+++ Assets/Bob.cs
@@ -4,11 +4,14 @@ class Bob: MonoBehaviour {
     public Vector3 amplitude = Vector3.zero;
     public Vector3 frequency = Vector3.zero;
     public Vector3 offset = Vector3.zero;
+    public Vector3 rotAmplitude = Vector3.zero;
+    public Vector3 rotFrequency = Vector3.zero;
+    public Vector3 rotOffset = Vector3.zero;
 
     Vector3 origin;
 
     void Start() {
-        origin = transform.position;
+        origin = transform.localPosition;
     }
 
     void Update() {
@@ -19,6 +22,13 @@ class Bob: MonoBehaviour {
         var baz = new Vector3(bar.x * amplitude.x,
                               bar.y * amplitude.y,
                               bar.z * amplitude.z);
-        transform.position = origin + baz;
+        transform.localPosition = origin + baz;
+        var t1 = rotFrequency * Time.time;
+        var t2 = new Vector3(Mathf.Sin(t1.x + rotOffset.x * Mathf.Deg2Rad),
+                             Mathf.Sin(t1.y + rotOffset.y * Mathf.Deg2Rad),
+                             Mathf.Sin(t1.z + rotOffset.z * Mathf.Deg2Rad));
+        transform.localEulerAngles = new Vector3(t2.x * rotAmplitude.x,
+                                                 t2.y * rotAmplitude.y,
+                                                 t2.z * rotAmplitude.z);
     }
 }
