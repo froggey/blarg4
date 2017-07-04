553156c4734874b68eb0f92a06d3a61f69290fd4
Henry Harrington <henry.harrington@gmail.com>
2017-07-04 21:50:14 +0100

    Fix sideways balloons in the lobby.

--- Assets/Bob.cs
+++ Assets/Bob.cs
@@ -4,9 +4,6 @@ class Bob: MonoBehaviour {
     public Vector3 amplitude = Vector3.zero;
     public Vector3 frequency = Vector3.zero;
     public Vector3 offset = Vector3.zero;
-    public Vector3 rotAmplitude = Vector3.zero;
-    public Vector3 rotFrequency = Vector3.zero;
-    public Vector3 rotOffset = Vector3.zero;
 
     Vector3 origin;
 
@@ -23,12 +20,5 @@ class Bob: MonoBehaviour {
                               bar.y * amplitude.y,
                               bar.z * amplitude.z);
         transform.localPosition = origin + baz;
-        var t1 = rotFrequency * Time.time;
-        var t2 = new Vector3(Mathf.Sin(t1.x + rotOffset.x * Mathf.Deg2Rad),
-                             Mathf.Sin(t1.y + rotOffset.y * Mathf.Deg2Rad),
-                             Mathf.Sin(t1.z + rotOffset.z * Mathf.Deg2Rad));
-        transform.localEulerAngles = new Vector3(t2.x * rotAmplitude.x,
-                                                 t2.y * rotAmplitude.y,
-                                                 t2.z * rotAmplitude.z);
     }
 }
